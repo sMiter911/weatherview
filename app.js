c8daf74e53f073b852f2bc4535cf9a02da3b5dce
Wandumuzi Mathunjwa (Platinum Life) <Wandumuzi.Mathunjwa@platinumlife.co.za>
2019-11-17 20:10:28 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const createError = require("http-errors");
+const express = require("express");
+const fetch = require("node-fetch");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+
+const app = express();
+
+// register environment variables
+require("dotenv").config();
+
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("trust proxy", true);
+
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", async (req, res, next) => {
+  const locKey = process.env.LOCATION_KEY;
+  const locResponse = await fetch(
+    `http://api.ipstack.com/${req.ip}?access_key=${locKey}`
+  );
+  const location = await locResponse.json();
+  console.log(location);
+
+  // weather request
+  const weatherKey = process.env.WEATHER_KEY;
+  const response = await fetch(
+    `http://api.weatherstack.com/current?access_key=${weatherKey}&query=${location.city}`
+  );
+  const weather = await response.json();
+
+  // respond
+  res.render("index", { title: "Weather app", weather, location });
+});
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404));
+});
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
+});
+
+module.exports = app;
